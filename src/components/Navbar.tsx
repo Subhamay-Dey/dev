f45aea57f7857ea652d712e1ef4a5fdd02a817ce
Subhamay-Dey <ncdey1966@gmail.com>
2025-10-11 23:59:35 +0530

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import MobileMenu from './MobileMenu'
+import Image from 'next/image'
 
 function Navbar() {
   return (
@@ -12,7 +13,12 @@ function Navbar() {
             </div>
 
             {/* CENTER */}
-            <div className='hidden'>
+            <div className='hidden md:flex'>
+                <div className=''>
+                    <Link href={"/"} className='flex gap-2'>
+                        <Image alt='Homepage' src={""} width={16} height={16}></Image>
+                    </Link>
+                </div>
 
             </div>
             {/* RIGHT */}
