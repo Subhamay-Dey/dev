b4794f2c4341b2f3ab7f3efdfa0b633d05462ca7
Subhamay-Dey <ncdey1966@gmail.com>
2025-10-13 02:33:34 +0530

    Update package dependencies and add middleware for protected routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,17 @@ function Navbar() {
     <>
         <div className='flex items-center justify-between h-24'>
             {/* LEFT */}
-            <div className='md:hidden lg:block'>
+            <div className='md:hidden lg:block w-[20%]'>
                 <Link href={"/"} className='font-bold text-xl text-blue-600'>DEVSOCIAL</Link>
             </div>
-
+ 
             {/* CENTER */}
-            <div className='hidden md:flex'>
-                <div className='flez gap-6'>
+            <div className='hidden md:flex w-[40%]'>
+                <div className='flex gap-6'>
                     <Link href={"/"} className='flex gap-2'>
                         {/* <Image alt='Homepage' src={"/home.png"} width={16} height={16}></Image> */}
                         <span>Homepage</span>
                     </Link>
-                
                     <Link href={"/"} className='flex gap-2'>
                         {/* <Image alt='Friends' src={""} width={16} height={16}></Image> */}
                         <span>Friends</span>
@@ -32,7 +31,7 @@ function Navbar() {
 
             </div>
             {/* RIGHT */}
-            <div className=''>
+            <div className='w-[30%] flex justify-end items-center gap-4 xl:gap-8'>
                 <MobileMenu/>
             </div>
 
