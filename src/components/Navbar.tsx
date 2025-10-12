cd9da8c9d38afa7f010655ea155328d31c44b874
Subhamay-Dey <ncdey1966@gmail.com>
2025-10-12 23:59:35 +0530

    navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,25 @@ function Navbar() {
     <>
         <div className='flex items-center justify-between h-24'>
             {/* LEFT */}
-            <div className=''>
+            <div className='md:hidden lg:block'>
                 <Link href={"/"} className='font-bold text-xl text-blue-600'>DEVSOCIAL</Link>
             </div>
 
             {/* CENTER */}
             <div className='hidden md:flex'>
-                <div className=''>
+                <div className='flez gap-6'>
                     <Link href={"/"} className='flex gap-2'>
-                        <Image alt='Homepage' src={""} width={16} height={16}></Image>
+                        {/* <Image alt='Homepage' src={"/home.png"} width={16} height={16}></Image> */}
+                        <span>Homepage</span>
+                    </Link>
+                
+                    <Link href={"/"} className='flex gap-2'>
+                        {/* <Image alt='Friends' src={""} width={16} height={16}></Image> */}
+                        <span>Friends</span>
+                    </Link>
+                    <Link href={"/"} className='flex gap-2'>
+                        {/* <Image alt='Stories' src={""} width={16} height={16}></Image> */}
+                        <span>Stories</span>
                     </Link>
                 </div>
 
