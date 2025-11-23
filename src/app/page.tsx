bf4ca3f8fa4642deaeda7e092af791388b7e6073
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-23 23:50:31 +0530

    Add LeftMenu component and integrate it into the Home page layout Left section ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import LeftMenu from "@/components/LeftMenu";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <div className="flex gap-6">
-    <div className="hidden xl:block w-[20%]"></div>
+    <div className="hidden xl:block w-[20%]"><LeftMenu/></div>
     <div className="w-full lg:w-[70%] xl:w-[50%]"></div>
     <div className="hidden lg:block w-[30%]"></div>
     </div>
