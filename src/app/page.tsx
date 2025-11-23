5014f02c1efdd489fc771aae8881ae3e37277561
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-23 23:51:35 +0530

    Added RightMenu component and integrate it into the Home page layout right section ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import LeftMenu from "@/components/LeftMenu";
+import RightMenu from "@/components/RightMenu";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
     <div className="flex gap-6">
     <div className="hidden xl:block w-[20%]"><LeftMenu/></div>
     <div className="w-full lg:w-[70%] xl:w-[50%]"></div>
-    <div className="hidden lg:block w-[30%]"></div>
+    <div className="hidden lg:block w-[30%]"><RightMenu/></div>
     </div>
     </>
   );
