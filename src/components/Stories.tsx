19da2030f0d593b8f9cb1acb5fd6515f7c4ac1b2
Subhamay-Dey <ncdey1966@gmail.com>
2025-11-29 02:47:26 +0530

    Enhance Stories component layout and add image handling in next.config.ts ✅

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Stories() {
   return (
-    <div className='p-4'>
+    <div className='p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs'>
+      <div className='flex gap-8'>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image src={"https://i.pinimg.com/1200x/c0/09/0e/c0090ee8e5cf9e2a257beea0e5fa489f.jpg"} alt="" width={80} height={80} className='w-20 h-20 rounded-full ring-2'/>
+          <span className='font-medium'>Ricky</span>
+        </div>
+      </div>
     </div>
   )
 }
