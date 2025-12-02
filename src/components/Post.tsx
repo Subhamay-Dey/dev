f65137a3a4f2217c80f8c9d7881f100a203d535b
Subhamay-Dey <ncdey1966@gmail.com>
2025-12-02 23:58:24 +0530

    Implement Post component layout with user display and structure for description and interaction ✅

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Post() {
   return (
-    <div>Post</div>
+    <div className='flex flex-col gap-4'>
+      {/*USER*/}
+      <div className=''>
+        <div className='flex items-center gap-4'>
+          <Image src={"https://i.pinimg.com/736x/ae/60/98/ae6098078360d7ec1cc2add3df692b3d.jpg"} alt='' width={40} height={40} className='w-10 h-10 rounded-full'/>
+          <span className='font-medium'>Subhamay Dey</span>
+        </div>
+      </div>
+      {/* DESC */}
+      <div className=''>
+
+      </div>
+      {/* INTERACTION */}
+      <div className=''>
+
+      </div>
+    </div>
   )
 }
 
