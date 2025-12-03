55260133b775e9981d4cc09c20c0f1a95b326220
Subhamay-Dey <ncdey1966@gmail.com>
2025-12-03 23:59:41 +0530

    Refactor Post component to improve user section layout with flex styling ✅

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,7 +5,7 @@ function Post() {
   return (
     <div className='flex flex-col gap-4'>
       {/*USER*/}
-      <div className=''>
+      <div className='flex items-center justify-between'>
         <div className='flex items-center gap-4'>
           <Image src={"https://i.pinimg.com/736x/ae/60/98/ae6098078360d7ec1cc2add3df692b3d.jpg"} alt='' width={40} height={40} className='w-10 h-10 rounded-full'/>
           <span className='font-medium'>Subhamay Dey</span>
