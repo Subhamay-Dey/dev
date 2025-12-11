51865232e06f5afa20d681f3c07b1539e463642e
Subhamay-Dey <ncdey1966@gmail.com>
2025-12-11 23:59:35 +0530

    Comment component under process ✅

---
+++ src/components/Comments.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Comments() {
+  return (
+    <div className=''>
+        <div className='flex items-center gap-4'>
+            <Image />
+        </div>
+    </div>
+  )
+}
+
+export default Comments
