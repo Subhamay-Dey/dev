31ef5ae04e963614ccaba7d1bbb424c344d2f8cb
Subhamay-Dey <ncdey1966@gmail.com>
2025-12-14 23:54:48 +0530

    Fix Image component in Comments to include necessary props for rendering ✅

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Comments() {
   return (
     <div className=''>
         <div className='flex items-center gap-4'>
-            <Image />
+            <Image src={""} alt='' width={32} height={32} className='w-8 h-8 rounded-full'/>
         </div>
     </div>
   )
