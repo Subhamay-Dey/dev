958abae2e66c2f0278c0d439033c116f3be6e1e9
Subhamay-Dey <ncdey1966@gmail.com>
2025-12-17 23:53:55 +0530

    update ✅

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -6,7 +6,7 @@ function Comments() {
     <div className='flex'>
         <div className='flex items-center gap-4'>
             <Image src={""} alt='' width={32} height={32} className='w-8 h-8 rounded-full'/>
-            
+            <div></div>
         </div>
     </div>
   )
